9c466dda4d0c51241a29063775aaec4b899cf01a
jonathancw-ecp <38476448+jonathancw-ecp@users.noreply.github.com>
2018-04-17 16:43:43 -0400

    Create test_conf.php

---
+++ test_conf.php
@@ -0,0 +1,13 @@
+<?php
+mysql_connect('host', 'username', 'password');
+mysql_select_db('database_name');
+
+$admin='admin';
+
+$url_home = 'index.php';
+
+$design = 'default';
+
+
+include('init.php');
+?>
